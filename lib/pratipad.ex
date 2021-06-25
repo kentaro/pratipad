cd0acf92c14ea7ff143382483cd665f929ffc7b2
Kentaro Kuribayashi <kentarok@gmail.com>
2021-06-25 10:10:17 +0900

    renew project

--- lib/pratipad.ex
+++ lib/pratipad.ex
@@ -8,24 +8,11 @@ defmodule Pratipad do
 
   ## Examples
 
-      iex> {:ok, left} = Pratipad.start_protocol(Pratipad.Protocol.GenServer, %{name: :left})
-      iex> {:ok, mid} = Pratipad.start_protocol(Pratipad.Protocol.GenServer, %{name: :mid})
-      iex> {:ok, right} = Pratipad.start_protocol(Pratipad.Protocol.GenServer, %{name: :right})
-      iex> left <|> mid <|> right
-      iex> GenServer.cast(:client, {:send, [client, "hello"]})
+      iex> Pratipad.hello()
+      :world
 
   """
-  defmacro left <|> right do
-    quote do
-      rname = GenServer.call(unquote(right), :name)
-      GenServer.cast(rname, {:subscribe, unquote(left)})
-      lname = GenServer.call(unquote(left), :name)
-      GenServer.cast(lname, {:subscribe, unquote(right)})
-      unquote(right)
-    end
-  end
-
-  def start_protocol(protocol, options) do
-    DynamicSupervisor.start_child(Pratipad.Supervisor, {protocol, options})
+  def hello do
+    :world
   end
 end
--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Pratipad.MixProject do
     [
       app: :pratipad,
       version: "0.1.0",
-      elixir: "~> 1.11",
+      elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
@@ -14,8 +14,7 @@ defmodule Pratipad.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger],
-      mod: {Pratipad.Application, []}
+      extra_applications: [:logger]
     ]
   end
 
@@ -24,7 +23,6 @@ defmodule Pratipad.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
-      {:gen_stage, "~> 1.1.0"}
     ]
   end
 end
