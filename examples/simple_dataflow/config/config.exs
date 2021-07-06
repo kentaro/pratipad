a40c165c766dc33a2196407b4a1952d8e0c199ca
Kentaro Kuribayashi <kentarok@gmail.com>
2021-07-07 00:46:30 +0900

    implement backward dataflow

--- examples/simple_dataflow/config/config.exs
+++ examples/simple_dataflow/config/config.exs
@@ -1,24 +1,40 @@
 import Config
 
 config :pratipad,
-  broadway: [
-    forward: [
-      producer: [
-        module:
-          {OffBroadwayOtpDistribution.Producer,
-           [
-             mode: :push,
-             receiver: [
-               name: :off_broadway_otp_distribution_receiver
-             ]
-           ]}
-      ],
-      processors: [
-        default: [concurrency: 1]
-      ],
-      batchers: [
-        default: [concurrency: 1, batch_size: 3]
-      ]
+  dataflow: SimpleDataflow.Dataflow,
+  forward: [
+    producer: [
+      module:
+        {OffBroadwayOtpDistribution.Producer,
+         [
+           mode: :push,
+           receiver: [
+             name: :pratipad_receiver_forward
+           ]
+         ]}
+    ],
+    processors: [
+      default: [concurrency: 1]
+    ],
+    batchers: [
+      default: [concurrency: 1, batch_size: 3]
     ]
   ],
-  dataflow: SimpleDataflow.Dataflow
+  backward: [
+    producer: [
+      module:
+        {OffBroadwayOtpDistribution.Producer,
+         [
+           mode: :push,
+           receiver: [
+             name: :pratipad_receiver_backward
+           ]
+         ]}
+    ],
+    processors: [
+      default: [concurrency: 1]
+    ],
+    batchers: [
+      default: [concurrency: 1, batch_size: 1]
+    ]
+  ]
