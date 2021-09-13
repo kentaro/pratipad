90767208e858aefafb2c90c62e78d26d6b14ed99
Kentaro Kuribayashi <kentarok@gmail.com>
2021-09-13 10:27:52 +0900

    add tests for other dataflow

--- test/pratipad/handler/message_test.ex
+++ test/pratipad/handler/message_test.ex
@@ -17,7 +17,49 @@ defmodule Pratipad.Handler.Message.Test do
     end
   end
 
-  test "simple" do
+  defmodule Add2 do
+    use Pratipad.Processor
+
+    @impl GenServer
+    def init(opts) do
+      {:ok, opts}
+    end
+
+    @impl Pratipad.Processor
+    def process(data, _state) do
+      data + 2
+    end
+  end
+
+  defmodule Add3 do
+    use Pratipad.Processor
+
+    @impl GenServer
+    def init(opts) do
+      {:ok, opts}
+    end
+
+    @impl Pratipad.Processor
+    def process(data, _state) do
+      data + 3
+    end
+  end
+
+  defmodule Add4 do
+    use Pratipad.Processor
+
+    @impl GenServer
+    def init(opts) do
+      {:ok, opts}
+    end
+
+    @impl Pratipad.Processor
+    def process(data, _state) do
+      data + 4
+    end
+  end
+
+  test "single dataflow" do
     dataflow = Push ~> Add1 ~> Output
     {:ok, handler} = Pratipad.Handler.Message.start_link(dataflow: dataflow)
 
@@ -25,5 +67,55 @@ defmodule Pratipad.Handler.Message.Test do
     processed = GenServer.call(handler, {:process, message})
 
     assert processed.data == 1
+  end
+
+  test "multiple dataflow" do
+    dataflow = Push ~> Add1 ~> Add2 ~> Output
+    {:ok, handler} = Pratipad.Handler.Message.start_link(dataflow: dataflow)
+
+    message = %Broadway.Message{data: 0, acknowledger: nil}
+    processed = GenServer.call(handler, {:process, message})
+
+    assert processed.data == 3
+  end
+
+  test "sequencial dataflow" do
+    dataflow = Push ~> [Add1, Add2] ~> Output
+    {:ok, handler} = Pratipad.Handler.Message.start_link(dataflow: dataflow)
+
+    message = %Broadway.Message{data: 0, acknowledger: nil}
+    processed = GenServer.call(handler, {:process, message})
+
+    assert processed.data == 3
+  end
+
+  test "parallel dataflow" do
+    dataflow = Push ~> {Add1, Add2} ~> Output
+    {:ok, handler} = Pratipad.Handler.Message.start_link(dataflow: dataflow)
+
+    message = %Broadway.Message{data: 0, acknowledger: nil}
+    processed = GenServer.call(handler, {:process, message})
+
+    assert processed.data == 0
+  end
+
+  test "conbination of sequencial and parallel dataflow" do
+    dataflow = Push ~> [Add1, Add2] ~> {Add3, Add4} ~> Output
+    {:ok, handler} = Pratipad.Handler.Message.start_link(dataflow: dataflow)
+
+    message = %Broadway.Message{data: 0, acknowledger: nil}
+    processed = GenServer.call(handler, {:process, message})
+
+    assert processed.data == 3
+  end
+
+  test "conbination of parallel and sequenciall dataflow" do
+    dataflow = Push ~> {Add1, Add2} ~> [Add3, Add4] ~> Output
+    {:ok, handler} = Pratipad.Handler.Message.start_link(dataflow: dataflow)
+
+    message = %Broadway.Message{data: 0, acknowledger: nil}
+    processed = GenServer.call(handler, {:process, message})
+
+    assert processed.data == 7
   end
 end
